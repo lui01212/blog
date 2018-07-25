f50fc8f183ec7150d9e577dd0fc6cb4dcc4eab38
lui01212 <bvl0165@gmail.com>
2018-07-25 17:08:24 +0700

    add 0725

--- resources/views/page/index.blade.php
+++ resources/views/page/index.blade.php
@@ -8,10 +8,10 @@
             <!-- Badges -->
                 <div class="block-header">
                     <div class="row">
-                        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                        <div class="col-lg-3 col-md-3 col-sm-7 col-xs-7">
                             <h2><i class="material-icons">whatshot</i>TRUYỆN HOT</h2>
                         </div>
-                        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 col-xs-offset-6 col-sm-offset-6 col-md-offset-6  col-lg-offset-6">
+                        <div class="col-lg-3 col-md-3 col-sm-5 col-xs-5 col-md-offset-6  col-lg-offset-6">
                             <select class="form-control">
                                     <option ></option>
                                     <option ></option>
@@ -28,28 +28,104 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-3 col-sm-4 col-xs-4">
-                    <ul class="list-group">
-                        <li class="list-group-item list-group-bg-teal">Tiên Hiệp<span class="badge bg-pink">14 new</span></li>
-                        <li class="list-group-item list-group-bg-teal">Huyền Huyễn<span class="badge bg-cyan">99 read</span></li>
-                        <li class="list-group-item list-group-bg-teal">Xuyên Không<span class="badge bg-teal">99+</span></li>
-                        <li class="list-group-item list-group-bg-teal">Đô Thị<span class="badge bg-orange">21</span></li>
-                        <li class="list-group-item list-group-bg-teal">Dị Giới<span class="badge bg-purple">18</span></li>
-                        <li class="list-group-item list-group-bg-teal">Cras justo odio <span class="badge bg-pink">14 new</span></li>
-                        <li class="list-group-item list-group-bg-teal">Dapibus ac facilisis in <span class="badge bg-cyan">99 read</span></li>
-                        <li class="list-group-item list-group-bg-teal">Morbi leo risus <span class="badge bg-teal">99+</span></li>
-                        <li class="list-group-item list-group-bg-teal">Porta ac consectetur ac <span class="badge bg-orange">21</span></li>
-                        <li class="list-group-item list-group-bg-teal">Vestibulum at eros <span class="badge bg-purple">18</span></li>
-                    </ul>
+                <div class="col-lg-2 col-md-3 col-sm-3 col-xs-3 m-t-5">
+                    <div class="list-group">
+                        <a href="javascript:void(0);" class="list-group-item">Tiên Hiệp</a>
+                        <a href="javascript:void(0);" class="list-group-item">Huyền Huyễn</a>
+                        <a href="javascript:void(0);" class="list-group-item">Đô Thị</a>
+                        <a href="javascript:void(0);" class="list-group-item">Võng Du</a>
+                        <a href="javascript:void(0);" class="list-group-item">Trọng Sinh</a>
+                        <a href="javascript:void(0);" class="list-group-item">Khoa Huyễn</a>
+                        <a href="javascript:void(0);" class="list-group-item">Đam Mỹ</a>
+                        <a href="javascript:void(0);" class="list-group-item">Tuyện Teen</a>
+                        <a href="javascript:void(0);" class="list-group-item">Sắc Hiệp</a>
+                        <a href="javascript:void(0);" class="list-group-item">Hài Hước</a>
+                        <a href="javascript:void(0);" class="list-group-item">Hài Hước</a>
+                    </div>
+                </div>
+                <div class="col-lg-8 col-md-6 col-sm-12 col-xs-12">
+                    <div class="row"> 
+                        <div class="card-carousel">
+                          <div class="my-card"> 1 </div>
+                          <div class="my-card"> 2 </div>
+                          <div class="my-card"> 3 </div>
+                          <div class="my-card"> 4 </div>
+                          <div class="my-card"> 5 </div>
+                          <div class="my-card"> 6 </div>
+                          <div class="my-card"> 7 </div>
+                        </div>
+                    </div>
+
+                    <div class="row m-t-25"> 
+                        <div class="column col-lg-2 col-md-2 col-sm-2 col-xs-2">
+                            <img class="hover-shadow" src="http://truyencv.com/images/poster/the-gamer-he-thong-poster-20170720-220x330.jpg" style="width: 100%" />
+                            <div class="column-contents">
+                                <h6 class="text-nowrap">The Gamer Hệ Thống</h6>
+                            </div>
+                            <a href="javascript:void(0);" class="btn bg-pink btn-block btn-xs waves-effect">Đọc</a>
+                        </div>
+                        <div class="column col-lg-2 col-md-2 col-sm-2 col-xs-2">
+                            <img class="hover-shadow" src="http://truyencv.com/images/poster/than-dao-dan-ton-poster-20161018-220x330.jpg" style="width: 100%" />
+                            <div class="column-contents">
+                                <h6>Thần Đạo Đan Tôn</h6>
+                            </div>
+                            <a href="javascript:void(0);" class="btn bg-pink btn-block btn-xs waves-effect">Đọc</a>
+                        </div>
+                        <div class="column col-lg-2 col-md-2 col-sm-2 col-xs-2">
+                            <img class="hover-shadow" src="http://truyencv.com/images/poster/yeu-nghiet-tien-hoang-tai-do-thi-poster-1531740108-200x300.jpg" style="width: 100%" />
+                            <div class="column-contents">
+                                <h6 class="text-truncate">Yêu Nghiệt Tiên Hoàng Tại Đô Thị</h6>
+                            </div>
+                            <a href="javascript:void(0);" class="btn bg-pink btn-block btn-xs waves-effect">Đọc</a>
+                        </div>
+                        <div class="column col-lg-2 col-md-2 col-sm-2 col-xs-2">
+                            <img class="hover-shadow" src="https://www.nae.vn/ttv/ttv/public/images/story/90cfd93e1e636628ca515cbe239effdaa44724df7bff46f3135f29c2b227f9f5.jpg" style="width: 100%" />
+                            <div class="column-contents">
+                                <h6>Phàm Nhân Tu Tiên Chi Tiên Giới Thiên - 凡人修仙之仙界篇</h6>
+                            </div>
+                            <a href="javascript:void(0);" class="btn bg-pink btn-block btn-xs waves-effect">Đọc</a>
+                        </div>
+                        <div class="column col-lg-2 col-md-2 col-sm-2 col-xs-2">
+                            <img class="hover-shadow" src="https://www.nae.vn/ttv/ttv/public/images/story/102.jpg" style="width: 100%" />
+                            <div class="column-contents">
+                                <h6>Tiên Lộ Xuân Thu</h6>
+                            </div>
+                            <a href="javascript:void(0);" class="btn bg-pink btn-block btn-xs waves-effect">Đọc</a>
+                        </div>
+                        <div class="column col-lg-2 col-md-2 col-sm-2 col-xs-2">
+                            <img class="hover-shadow" src="https://www.nae.vn/ttv/ttv/public/images/story/190.jpg" style="width: 100%" />
+                            <div class="column-contents">
+                                <h6>Vạn Cổ Tiên Khung</h6>
+                            </div>
+                            <a href="javascript:void(0);" class="btn bg-pink btn-block btn-xs waves-effect">Đọc</a>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-lg-2 col-md-3 col-sm-3 col-xs-3 m-t-5">
+                    <div class="list-group">
+                        <a href="javascript:void(0);" class="list-group-item">Tiên Hiệp</a>
+                        <a href="javascript:void(0);" class="list-group-item">Huyền Huyễn</a>
+                        <a href="javascript:void(0);" class="list-group-item">Đô Thị</a>
+                        <a href="javascript:void(0);" class="list-group-item">Võng Du</a>
+                        <a href="javascript:void(0);" class="list-group-item">Trọng Sinh</a>
+                        <a href="javascript:void(0);" class="list-group-item">Khoa Huyễn</a>
+                        <a href="javascript:void(0);" class="list-group-item">Đam Mỹ</a>
+                        <a href="javascript:void(0);" class="list-group-item">Tuyện Teen</a>
+                        <a href="javascript:void(0);" class="list-group-item">Sắc Hiệp</a>
+                        <a href="javascript:void(0);" class="list-group-item">Hài Hước</a>
+                        <a href="javascript:void(0);" class="list-group-item">Hài Hước</a>
+                    </div>
                 </div>
             <!-- #END# Badges -->
        </div>
     </div>
 </div>
 @endsection
-- #END# Content -->
+<!-- #END# Content -->
 
 <!-- Custom Js -->
 @section('customJs')
+<script src="{{ asset('js/index.js') }}"></script>
 @endsection
 <!-- #END# Custom Js -->
